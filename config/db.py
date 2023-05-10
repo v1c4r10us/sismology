8aaeaedce8bdb0f09f6152cdd66dde6a176ed83d
v1c4r10us <v1c4r10us.29@gmail.com>
2023-05-10 17:59:00 -0500

    adding environment variables

--- config/db.py
+++ config/db.py
@@ -1,4 +1,3 @@
-import os
 from pymongo import MongoClient
 
-conn = MongoClient(os.environ['MONGO_CONN'])
+conn = MongoClient(os.environ['mongodb+srv://v1c4r10us:tC6A4lWUzfFFJg9y@cluster0.cisx8.mongodb.net/?retryWrites=true&w=majority'])
